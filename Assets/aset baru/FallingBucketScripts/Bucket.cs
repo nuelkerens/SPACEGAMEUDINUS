9e97a62fc18e575d881c3a63afcc230dec888b02
nuelkerens <45616139+nuelkerens@users.noreply.github.com>
2019-12-20 00:32:02 +0700

    ema losss
    aset baru

---
+++ Assets/aset baru/FallingBucketScripts/Bucket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.name.Equals ("Cat"))
+			rb.isKinematic = false;
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.name.Equals ("Cat"))
+			Debug.Log ("Got you!");
+	}
+
+}
---
+++ Assets/aset baru/FallingBucketScripts/MoveCat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCat : MonoBehaviour {
+
+	float dirX;
+	[SerializeField]
+	float moveSpeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		dirX = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+		transform.position = new Vector2 (transform.position.x + dirX, transform.position.y);
+	}
+}
